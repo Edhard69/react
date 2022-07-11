5dd2be49a33f18d14c3d42831dc43712a3635072
Edgar <eaguila@almata.cat>
2022-07-11 17:45:07 +0200

    first commit

---
+++ test/src/App.js
@@ -0,0 +1,26 @@
+//import logo from './logo.svg';
+import logo from './barbie.png';
+
+
+import './App.css';
+
+function App() {
+	
+  return (
+  
+    <div className="App">
+      <header className="App-header">
+		<br></br><br></br><br></br><br></br><br></br>
+        <img src={logo} className="App-logo" alt="logo" />
+		<br></br><br></br><br></br><br></br><br></br>
+			<h1 class="HW">Hello world!</h1>
+
+		
+      </header>
+    </div>
+	
+  );
+  
+}
+
+export default App;
